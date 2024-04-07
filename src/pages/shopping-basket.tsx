0c00327aa288fcf6dedda293ac9cf4734b44c96e
Steven Mbiamy <mbiamysteven@gmail.com>
2024-04-07 09:38:37 +0100

    basket 40%

--- src/pages/shopping-basket.tsx
+++ src/pages/shopping-basket.tsx
@@ -10,22 +10,22 @@ import ShoppingCart2 from "../components/cart/shoppingCart2";
 export default function ShoppingBasket() {
 	let cartItems = [];
 	data.shoppingCart.map(id => data.products.filter(x => x.id == id).map(x => cartItems.push(x)))
-	
-	let cartItems2 = [];
-data.shoppingCart2.map(id => 
-  data.products.filter(x => x.id == id).map(x => cartItems2.push(x)))
-  
-	return <>
-	<div className="my-0">
-        <ShoppingCart
-          products = {cartItems}
-        />
-      </div>
 
-	  <div className="my-0">
-        <ShoppingCart2
-          products = {cartItems2}
-        />
-      </div>
-	  </>
+	let cartItems2 = [];
+	data.shoppingCart2.map(id =>
+		data.products.filter(x => x.id == id).map(x => cartItems2.push(x)))
+
+	return <>
+		<div className="my-0">
+			<ShoppingCart
+				products={cartItems}
+			/>
+		</div>
+
+		<div className="my-0">
+			<ShoppingCart2
+				products={cartItems2}
+			/>
+		</div>
+	</>
 }
