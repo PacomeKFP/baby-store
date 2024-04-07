16d3a912a0e7edce40e6c2dba0c4831ec271f3f3
Steven Mbiamy <mbiamysteven@gmail.com>
2024-04-07 07:27:47 +0100

    shopping-basket 40%

--- src/pages/shopping-basket.tsx
+++ src/pages/shopping-basket.tsx
@@ -1,6 +1,31 @@
+import ShoppingCart from "../components/cart/shoppingCart";
+import data from '../../public/data.json';
+import ShoppingCart2 from "../components/cart/shoppingCart2";
+
+
+
 /*
 * La page qui permettra de visualiser le panier de l'utilisateur.
 *  */
 export default function ShoppingBasket() {
-	return <>Panier de l'utilisateur</>
+	let cartItems = [];
+	data.shoppingCart.map(id => data.products.filter(x => x.id == id).map(x => cartItems.push(x)))
+	
+	let cartItems2 = [];
+data.shoppingCart2.map(id => 
+  data.products.filter(x => x.id == id).map(x => cartItems2.push(x)))
+  
+	return <>
+	<div className="my-0">
+        <ShoppingCart
+          products = {cartItems}
+        />
+      </div>
+
+	  <div className="my-0">
+        <ShoppingCart2
+          products = {cartItems2}
+        />
+      </div>
+	  </>
 }
